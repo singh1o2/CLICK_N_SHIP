2682a3c6fe01cf12c18e6eac68c1dd1cef4591bb
singh1o2 <58059441+singh1o2@users.noreply.github.com>
2020-10-25 22:16:13 -0400

    Update Inventory.js

--- static/Inventory.js
+++ static/Inventory.js
@@ -1,1 +1,20 @@
-console.log("hi");
+buttons = document.querySelectorAll('.dropbtn');
+
+for (var i = 0; i <buttons.length; i++) {
+      console.log(buttons);
+      buttons[i].addEventListener('click',function()
+      {
+         card = this.nextSibling.nextSibling;
+         if( card.style.display == 'flex')
+         {
+            console.log(card);
+            card.style.display = 'none';
+         }
+        else
+        {
+            console.log(card.style.display);
+            card.style.display = 'flex';
+        }
+         document.querySelector('body').style.height = '100%';
+      });
+}
