7a0020f10329383552f7d2e51ce705843e4c0edb
singh1o2 <58059441+singh1o2@users.noreply.github.com>
2020-11-16 20:20:30 -0500

    Update ShoppingCart.js

--- static/ShoppingCart.js
+++ static/ShoppingCart.js
@@ -1,4 +1,4 @@
-if(localStorage != null)
+/*if(localStorage != null)
 {
     var Products =JSON.parse(localStorage.ProductStoredTemp);
     Node = document.querySelector('.card-group');
@@ -35,4 +35,48 @@ function displayText(NoOfProducts)
   {
      document.querySelector('h1').style.display = 'flex';
   }
+}*/
+
+
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
 }
+const csrf_token = getCookie('csrftoken');
+
+
+Products = document.querySelectorAll('.card');
+Price =0;
+for(i=0;i<Products.length;i++)
+{
+    Button = Products[i].querySelector('button');
+    productDescription= Products[i].querySelector('.card-text').textContent;
+    Price+=parseInt(productDescription);
+    console.log(Price);
+    Button.addEventListener('click',function()
+    {
+      this.parentElement.parentElement.style.display = 'none';
+      Price-=parseInt(this.parentElement.querySelector('.card-text').textContent);
+      document.querySelector('#Amount .card-text').textContent = "TOTAL AMOUNT    :    "+Price;
+      $.ajax({
+        headers: { "X-CSRFToken": csrf_token },
+        type:'POST',
+        csrfmiddlewaretoken: '{{ csrf_token }}',
+        url:'../deletefromCart/',
+        data: JSON.stringify(this.parentElement.querySelector('.card-title').textContent),
+        datatype:'json'
+      });
+    });
+  }
+document.querySelector('#Amount .card-text').textContent = "TOTAL AMOUNT    :    "+Price;
