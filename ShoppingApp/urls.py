ba5f4b516495c476384aaea7adacdca9eb1900ba
singh1o2 <58059441+singh1o2@users.noreply.github.com>
2020-11-16 20:20:09 -0500

    Update urls.py

--- ShoppingApp/urls.py
+++ ShoppingApp/urls.py
@@ -3,5 +3,8 @@ from ShoppingApp import views
 urlpatterns = [
     path('ShoppingCart/',views.ShoppingCart,name = 'ShoppingCart'),
     path('Inventory/',views.Inventory,name = 'Inventory'),
+    path('addToCart/',views.addToCart,name = 'addToCart'),
+    path('Seller/',views.Seller,name = 'Seller'),
+    path('deletefromCart/',views.deletefromCart,name = 'deletefromCart'),
     path('',views.Index,name = 'Index')
 ]
