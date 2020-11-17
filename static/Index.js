7d3ce85f666b894b25ef2610529a26938bececed
singh1o2 <58059441+singh1o2@users.noreply.github.com>
2020-11-16 20:20:23 -0500

    Update Index.js

--- static/Index.js
+++ static/Index.js
@@ -1,4 +1,4 @@
-Products = document.querySelectorAll('.card');
+/*Products = document.querySelectorAll('.card');
 class Product{
   PID;
   ProductName;
@@ -27,5 +27,50 @@ for(i=0;i<Products.length;i++)
         localStorageProducts[index]=ActualProducts[x];
         localStorage.ProductStoredTemp = JSON.stringify(localStorageProducts);
         index++;
+    });
+}
+**/
+
+//
+
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+const csrf_token = getCookie('csrftoken');
+$("body").bind("ajaxSend", function(elm, xhr, s){
+   if (s.type == "POST") {
+      xhr.setRequestHeader('X-CSRF-Token', csrf_token);
+   }
+})
+
+Products = document.querySelectorAll('.card');
+for(i=0;i<Products.length;i++)
+{
+    Button = Products[i].querySelector('button');
+    $(Button).click(function(e)
+    {
+      productName = this.parentElement.querySelector('.card-title').textContent;
+      console.log(productName);
+      e.preventDefault();
+      $.ajax({
+        headers: { "X-CSRFToken": csrf_token },
+        type:'POST',
+        csrfmiddlewaretoken: '{{ csrf_token }}',
+        url:'../addToCart/',
+        data: JSON.stringify(productName),
+        datatype:'json'
+      });
     });
 }
