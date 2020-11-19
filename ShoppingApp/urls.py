350ebb161a2d706238b22d25b8c26fcd1f2a9c0d
singh1o2 <58059441+singh1o2@users.noreply.github.com>
2020-11-18 19:54:17 -0500

    Update urls.py

--- ShoppingApp/urls.py
+++ ShoppingApp/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('addToCart/',views.addToCart,name = 'addToCart'),
     path('Seller/',views.Seller,name = 'Seller'),
     path('deletefromCart/',views.deletefromCart,name = 'deletefromCart'),
+    #path('Products/',views.Products,name = 'Product'),
+    path('viewProduct/',views.viewProduct,name = 'viewProduct'),
     path('',views.Index,name = 'Index')
 ]
