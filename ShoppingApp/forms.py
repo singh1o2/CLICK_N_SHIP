145bede2661ca3093f74818053e5ccf2fdc1eaf8
singh1o2 <58059441+singh1o2@users.noreply.github.com>
2020-12-01 22:41:52 -0500

    Update forms.py

--- ShoppingApp/forms.py
+++ ShoppingApp/forms.py
@@ -1,8 +1,13 @@
 from django import forms
-from ShoppingApp.models import Product
-
+from ShoppingApp.models import Product,User
+from django.contrib.auth.models import User
 
 class SellerForm(forms.ModelForm):
     class Meta:
         model = Product
         fields =['produtId','productName','productPrice','productDescription','productColor','productSellerName','productImage','ProductCategory',]
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+    class Meta():
+        model = User
+        fields = ['username','email','password']
