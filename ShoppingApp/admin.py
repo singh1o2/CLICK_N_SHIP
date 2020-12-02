75558b97492e3d8cd13942e5e31c4c2eac7aeea4
singh1o2 <58059441+singh1o2@users.noreply.github.com>
2020-12-01 22:41:49 -0500

    Update admin.py

--- ShoppingApp/admin.py
+++ ShoppingApp/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from ShoppingApp.models import Product,Cart
+from ShoppingApp.models import Product,Cart,User
 # Register your models here.
 admin.site.register(Product)
 admin.site.register(Cart)
+admin.site.register(User)
