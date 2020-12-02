65537704e46fa6858984a683c5e828b90f14a8b0
singh1o2 <58059441+singh1o2@users.noreply.github.com>
2020-12-01 22:42:40 -0500

    Update views.py

--- ShoppingApp/views.py
+++ ShoppingApp/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
-from ShoppingApp.models import Product,Cart,product,index
-from ShoppingApp.forms import SellerForm
+from ShoppingApp.models import Product,Cart,product,index,User
+from ShoppingApp.forms import SellerForm,UserForm
 from ShoppingApp import models
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 import json,os
+
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate,login,logout
 # Create your views here.s
 def Index(request): # for main page
     productList  = Product.objects.all()
@@ -47,16 +51,51 @@ def viewProduct(request):
     else:
         return render(request,'viewProduct.html',{'item': models.product[models.index]})
 
+def Register(request):
+    registered = False
+    if request.method == 'POST':
+        user_form = UserForm(data = request.POST)
+        if user_form.is_valid():
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save()
+            registered = True
+    else:
+        user_form = UserForm()
 
+    return render(request,'Register.html',{'user_form':user_form,'registered':registered})
+
+def userLogin(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username = username,password = password)
+
+        if user:
+            if user.is_active:
+                login(request,user)
+                return HttpResponseRedirect(reverse('Index'))
+            else:
+                return HttpResponse('user not active ...')
+        else:
+            return render(request,'Login.html')
+
+    else:
+        return render(request,'Login.html')
+
+@login_required
+def userLogout(request):
+    logout(request)
+    return HttpResponseRedirect('..')
 
 def addToCart(request):
-    productName =json.loads(request.body)
+    productName= json.loads(request.body)
     productList  = Product.objects.all()
     for r in productList:
         if(r.productName==productName):
             item = r
-    Cart.objects.create(product=item, productQuantity=1)
-    return HttpResponse("hi")
+            Cart.objects.create(product=item, productQuantity=1)
+    return HttpResponse("Item added to Car")
 
 
 def deletefromCart(request):
